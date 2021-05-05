d94c8ab404f40078a380171edd538001dafe7626
mumuzi233 <815001314@qq.com>
2021-05-06 02:18:49 +0800

    3.0

--- src/asdaa.java
+++ src/asdaa.java
@@ -2,5 +2,6 @@ public class asdaa {
     public static void main(String[] args) {
         System.out.println("update");
         System.out.println("version2.0");
+        System.out.println("version3.0");
     }
 }
